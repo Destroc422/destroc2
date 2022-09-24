182850f48005bc626f0b1f027f0153bae6120746
Destroc422 <114114885+Destroc422@users.noreply.github.com>
2022-09-24 12:40:40 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,22 @@
+input.onButtonPressed(Button.AB, function () {
+	
+})
+input.onGesture(Gesture.LogoDown, function () {
+	
+})
+music.setVolume(255)
+if (true) {
+    for (let index = 0; index < randint(1, 10); index++) {
+        music.playMelody("C D E E D E G F ", 120)
+        music.playMelody("E D F A B A G F ", 120)
+        music.playMelody("D C G A E G C E ", 120)
+        music.playMelody("G A B A G F E D ", 120)
+        music.playMelody("F G A G F E D C ", 120)
+        music.playMelody("D C G A E G C E ", 120)
+        music.playMelody("G A B A G F E D ", 120)
+        music.playMelody("F G A G F E D C ", 120)
+        music.playMelody("D C G A E G C E ", 120)
+        music.playMelody("E D F A B A G F ", 120)
+        music.playMelody("C D E E D E G F ", 120)
+    }
+}
